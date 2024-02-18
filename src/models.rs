463dcf9139b3fbab5b866ff42dce116563fecd7d
Ninthsun <ninthsun91@gmail.com>
2024-02-19 03:39:13 +0900

    1.1 define db models

---
+++ src/models.rs
@@ -0,0 +1,48 @@
+use std::collections::HashMap;
+
+pub enum Status {
+    Open,
+    InProgress,
+    Resolved,
+    Closed,
+}
+
+pub struct Epic {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+    pub stories: Vec<u32>,
+}
+
+impl Epic {
+    pub fn new(name: String, description: String) -> Self {
+        Epic {
+            name,
+            description,
+            status: Status::Open,
+            stories: vec![],
+        }
+    }
+}
+
+pub struct Story {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+}
+
+impl Story {
+    pub fn new(name: String, description: String) -> Self {
+        Story {
+            name,
+            description,
+            status: Status::Open,
+        }
+    }
+}
+
+pub struct DBState {
+    pub last_item_id: u32,
+    pub epics: HashMap<u32, Epic>,
+    pub stories: HashMap<u32, Story>,
+}
