52977bc7ed532757212b4df48b991f71252607a6
Ninthsun <ninthsun91@gmail.com>
2024-02-19 23:52:44 +0900

    1.2 apply serde traits to models

--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,8 @@
 use std::collections::HashMap;
 
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub enum Status {
     Open,
     InProgress,
@@ -7,6 +10,7 @@ pub enum Status {
     Closed,
 }
 
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct Epic {
     pub name: String,
     pub description: String,
@@ -25,6 +29,7 @@ impl Epic {
     }
 }
 
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct Story {
     pub name: String,
     pub description: String,
@@ -41,6 +46,7 @@ impl Story {
     }
 }
 
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct DBState {
     pub last_item_id: u32,
     pub epics: HashMap<u32, Epic>,
