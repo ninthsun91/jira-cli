cbfd019aecc80764cca6d8fa57f4ffa9fb0a1221
Ninthsun <ninthsun91@gmail.com>
2024-02-22 09:29:36 +0900

    2.1 add enum Action

--- src/models.rs
+++ src/models.rs
@@ -3,6 +3,20 @@ use std::fmt::Display;
 
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, PartialEq)]
+pub enum Action {
+    NavigateToEpicDetail { epic_id: u32 },
+    NavigateToStoryDetail { epic_id: u32, story_id: u32 },
+    NativateToPreviousPage,
+    CreateEpic,
+    UpdateEpicStatus { epic_id: u32 },
+    DeleteEpic { epic_id: u32 },
+    CreateStory { epic_id: u32 },
+    UpdateStoryStatus { story_id: u32 },
+    DeleteStory { epic_id: u32, story_id: u32 },
+    Exit,
+}
+
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 pub enum Status {
     Open,
