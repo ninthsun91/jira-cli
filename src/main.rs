f160a9835792b145fa41b332629d9612b59d3895
Ray Kim <ninthsun91@gmail.com>
2024-03-04 03:48:23 +0900

    3.2 main todo template

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
-mod db;
-mod io_utils;
+use std::rc::Rc;
+
 mod models;
-mod navigator;
+
+mod db;
+use db::*;
+
 mod ui;
 
+mod io_utils;
+use io_utils::*;
+
+mod navigator;
+use navigator::*;
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: create database and navigator
+    
+    loop {
+        clearscreen::clear().unwrap();
+
+        // TODO: implement the following functionality:
+        // 1. get current page from navigator. If there is no current page exit the loop.
+        // 2. render page
+        // 3. get user input
+        // 4. pass input to page's input handler
+        // 5. if the page's input handler returns an action let the navigator process the action
+    }
 }
